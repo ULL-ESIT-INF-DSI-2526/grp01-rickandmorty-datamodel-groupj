4654e5eea6fd6db931927dacbce71ab17b523bd8
Juan Pablo Calzadilla Gonzalez <alu0101651736@ull.edu.es>
2026-03-19 11:39:32 +0000

    comentario de clase

--- src/localizacion.ts
+++ src/localizacion.ts
@@ -1,6 +1,15 @@
 import { ILocalizacion } from "./interfaces";
 import { tipoLocalizacion } from "./types";
 
+/**
+ * Clase que representa una localizacion en el sistema, implementa la interfaz ILocalizacion.
+ * @param id - Identificador único de la localizacion
+ * @param nombre - Nombre de la localizacion
+ * @param tipo - Tipo de localizacion
+ * @param dimension - ID de la dimension origen
+ * @param poblacion - Poblacion aproximada
+ * @param descripcion - Descripción adicional de la localizacion
+ */
 export class Localizacion implements ILocalizacion {
   constructor(
     public readonly id: string,
@@ -17,10 +26,10 @@ export class Localizacion implements ILocalizacion {
       throw new Error("El nombre no puede ser vacio");
     }
     if (!dimension || dimension.trim().length === 0) {
-      throw new Error("El ID de referencia a dimension no puede ser vacio")
+      throw new Error("El ID de referencia a dimension no puede ser vacio");
     }
     if (poblacion < 1) {
-      throw new Error("Cantidad de poblacion invalida")
+      throw new Error("Cantidad de poblacion invalida");
     }
     this.id = id;
     this.nombre = nombre;
