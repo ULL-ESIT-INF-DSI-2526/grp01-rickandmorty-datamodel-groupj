99d672c3091b537e1a6dd267bfa03dfa25de2a23
Juan Pablo Calzadilla Gonzalez <alu0101651736@ull.edu.es>
2026-03-19 01:10:05 +0000

    constructor de localizacion

---
+++ src/localizacion.ts
@@ -0,0 +1,20 @@
+import { ILocalizacion } from "./interfaces";
+import { tipoLocalizacion } from "./types";
+
+export class Localizacion implements ILocalizacion {
+  constructor(
+    public readonly id: string,
+    public nombre: string,
+    public tipo: tipoLocalizacion,
+    public dimension: string,
+    public poblacion: number,
+    public descripcion: string,
+  ) {
+    this.id = id;
+    this.nombre = nombre;
+    this.tipo = tipo;
+    this.dimension = dimension;
+    this.poblacion = poblacion;
+    this.descripcion = descripcion;
+  }
+}
